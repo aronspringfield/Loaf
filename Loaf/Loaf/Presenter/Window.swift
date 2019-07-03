6c304125a1bc385666135c3efb8138b81dc34da5
Aron Springfield <aron@palringo.com>
2019-07-03 15:49:38 +0100

    Create a RootViewController so we can control the preferred status bar style via swizzling

--- Loaf/Loaf/Presenter/Window.swift
+++ Loaf/Loaf/Presenter/Window.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class Window: UIWindow {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.rootViewController = UIViewController()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         for view in subviews {
